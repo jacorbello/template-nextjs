26fd57240aa459d7b82f9bd0655f18a2d2435f90
Jeremy Corbello <jcorbello@tlainnovation.com>
2023-03-09 17:28:23 -0600

    Adding "react/jsx-key" to eslint bypass due to unexpected issue with array declaration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  "extends": [
-    "next/core-web-vitals",
-    "plugin:storybook/recommended",
-    "prettier"
-  ]
+  extends: ["next/core-web-vitals", "plugin:storybook/recommended", "prettier"],
+  rules: {
+    // TODO: Remove the 'react/jsx-key' bypass when global issue is fixed:
+    //    Similar to: https://github.com/jsx-eslint/eslint-plugin-react/issues/3215
+    //    Triggering on `/src/pages/index.tsx` on declaration of `commands` array.
+    "react/jsx-key": "off",
+  },
 };
